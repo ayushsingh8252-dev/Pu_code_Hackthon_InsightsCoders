9f2de9a17c62f6a590bd169b418b0cfddb5f0b9d
Venkatesh Rapolu <124189195+Venkatesh0768@users.noreply.github.com>
2026-01-23 18:14:18 +0530

    Backend: finalize vendor changes

--- backend/src/main/java/org/pucodehackathon/backend/vendor/dto/vendorDto/VendorUserProfileDto.java
+++ backend/src/main/java/org/pucodehackathon/backend/vendor/dto/vendorDto/VendorUserProfileDto.java
@@ -2,15 +2,11 @@ package org.pucodehackathon.backend.vendor.dto.vendorDto;
 
 import lombok.Builder;
 import lombok.Getter;
-
-import java.time.LocalDateTime;
-import java.util.Set;
 import java.util.UUID;
 
 @Getter
 @Builder
 public class VendorUserProfileDto {
-
     private UUID userId;
     private String email;
     private String firstName;
